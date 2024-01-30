91aac9b302fe8170b6e2b933a816327a9e4a870e
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-30 06:49:08 -0400

    api address to api.around...

--- src/utils/api.js
+++ src/utils/api.js
@@ -177,7 +177,7 @@ class Api {
 }
 
 const api = new Api({
-  address: "https://around.myremotetest.eu", //antiguo "https://around.nomoreparties.co/v1/web_es_09"
+  address: "https://api.around.myremotetest.eu", //antiguo "https://around.nomoreparties.co/v1/web_es_09"
   headers: {
     authorization:
       "Bearer bc803120d5a3d713089794c6b5fd8258f889fa26de704c44d90b8bc9243fedaf", // antes "24db7356-9f7a-470a-979e-9ec3f25f6f02 ")}`, // deberia ser "bc803120d5a3d713089794c6b5fd8258f889fa26de704c44d90b8bc9243fedaf", // antes "24db7356-9f7a-470a-979e-9ec3f25f6f02"
@@ -186,14 +186,14 @@ const api = new Api({
 });
 
 const apiRegister = new Api({
-  address: "https://around.myreomtetest.eu", // antiguo "https://register.nomoreparties.co"
+  address: "https://api.around.myreomtetest.eu", // antiguo "https://register.nomoreparties.co"
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 const apiToken = new Api({
-  address: "https://around.myremotetest.eu", //antiguo "https://register.nomoreparties.co"
+  address: "https://api.around.myremotetest.eu", //antiguo "https://register.nomoreparties.co"
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${localStorage.getItem("token")}`,
